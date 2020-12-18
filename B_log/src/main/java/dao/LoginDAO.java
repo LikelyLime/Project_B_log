a04b8347d70aaf44f9223e7cea8a5549df6e6fe3
qorwl <qorwl@DESKTOP-1HFCURL>
2020-12-18 12:43:13 +0900

    add_mapper,vo,dao

---
+++ B_log/src/main/java/dao/LoginDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import vo.LoginVO;
+
+public class LoginDAO {
+	SqlSession sqlsession;
+	public void setSqlsession(SqlSession sqlsession) {
+		this.sqlsession = sqlsession;
+	}
+	
+	public LoginVO selectOne( String id ) {
+		LoginVO vo = null;
+		vo = sqlsession.selectOne("b.id_search", id)
+		return "";
+	}
+	
+}
